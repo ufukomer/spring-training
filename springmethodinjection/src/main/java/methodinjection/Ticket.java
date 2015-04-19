ff66d81b5b3f2218f7486c756e37869ba580e9eb
unknown <ufukomer@hotmail.com>
2015-04-19 18:13:42 +0300

    Method Injection initialized

---
+++ springmethodinjection/src/main/java/methodinjection/Ticket.java
@@ -0,0 +1,7 @@
+package methodinjection;
+
+/**
+ * Created by Ufuk on 19-04-15.
+ */
+public class Ticket {
+}
---
+++ springmethodinjection/src/main/java/methodinjection/TicketVendingMachine.java
@@ -0,0 +1,8 @@
+package methodinjection;
+
+/**
+ * Created by Ufuk on 19-04-15.
+ */
+public abstract class TicketVendingMachine {
+    public abstract Ticket generateTicket();
+}
